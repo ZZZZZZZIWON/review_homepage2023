c810b30b48740ccd019e399f4cfdfc4fe98249eb
박지원 <pjw20120436@gmail.com>
2023-04-06 21:56:28 +0900

    0406 21:56

--- homepage2023/src/main/java/egovframework/let/temp/service/impl/TempServiceImpl.java
+++ homepage2023/src/main/java/egovframework/let/temp/service/impl/TempServiceImpl.java
@@ -1,5 +1,7 @@
 package egovframework.let.temp.service.impl;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Service;
 import egovframework.let.temp.service.TempService;
 import egovframework.let.temp.service.TempVO;
 import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 @Service("tempService")
 public class TempServiceImpl extends EgovAbstractServiceImpl implements TempService{
@@ -25,4 +28,17 @@ public class TempServiceImpl extends EgovAbstractServiceImpl implements TempServ
 		public TempVO selectTemp(TempVO vo) throws Exception {
 			return tempMapper.selectTemp(vo);
 		}
+		
+		// 임시 데이터 목록 가져오기
+		public List<EgovMap> selectTempList(TempVO vo) throws Exception {
+			return tempMapper.selectTempList(vo);
+		}
+		
+		// 임시 데이터 등록하기
+		public String insertTemp(TempVO vo) throws Exception {
+			tempMapper.insertTemp(vo);
+			return null;
+			
+		}
+		
 }
--- homepage2023/src/main/java/egovframework/let/temp/web/TempController.java
+++ homepage2023/src/main/java/egovframework/let/temp/web/TempController.java
@@ -1,5 +1,7 @@
 package egovframework.let.temp.web;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import egovframework.let.temp.service.TempService;
 import egovframework.let.temp.service.TempVO;
+import egovframework.rte.fdl.string.EgovStringUtil;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 @Controller
 public class TempController {
@@ -16,12 +20,43 @@ public class TempController {
 	@Resource(name="tempService")
 	private TempService tempService;
 
-	// 임시데이터 가져오기
+	// 임시 데이터 가져오기
 	@RequestMapping(value="/temp/select.do")
 	public String select(TempVO tempVO, HttpServletRequest request, ModelMap model) throws Exception {
 		TempVO result = tempService.selectTemp(tempVO);
 		model.addAttribute("result",result);
 		return "temp/TempSelect";
+	}
+	
+	// 임시 데이터 목록 가져오기
+	@RequestMapping(value="/temp/selectList.do")
+	public String selectList(TempVO tempVO, HttpServletRequest request, ModelMap model) throws Exception {
+		
+		List<EgovMap> resultList = tempService.selectTempList(tempVO);
+		model.addAttribute("resultList", resultList);
+		
+		return "temp/TempSelectList"; //jsp의 이름과 동일해야 함(반환값이기 때문에)
+	}
+	
+	// 임시데이터 등록 / 수정
+	@RequestMapping(value="/temp/tempRegist.do")
+	public String tempRegist(TempVO tempVO, HttpServletRequest request, ModelMap model) throws Exception {
+		TempVO result = new TempVO();
+		// egovframework.let.utl.fcc.service.EgovStringUti
+		if(!EgovStringUtil.isEmpty(tempVO.getTempId())) {
+			result = tempService.selectTemp(tempVO);
+		}
+		model.addAttribute("result", result);
+		
+		return "temp/TempRegist";
+	}
+	
+	// 임시 데이터 등록하기
+	@RequestMapping(value="/temp/insert.do")
+	public String insert(TempVO tempVO, HttpServletRequest request) throws Exception {
+		tempService.insertTemp(tempVO);
+		
+		return "forward:/temp/selectList.do";
 	}
 
 }
