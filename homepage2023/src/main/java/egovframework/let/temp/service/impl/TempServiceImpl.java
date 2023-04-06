e20e00a69d1bef6ffce3f6d0e0b92bf7c1d4cf8d
박지원 <pjw20120436@gmail.com>
2023-04-06 21:56:28 +0900

    0412 21:07

--- homepage2023/src/main/java/egovframework/let/temp/service/impl/TempServiceImpl.java
+++ homepage2023/src/main/java/egovframework/let/temp/service/impl/TempServiceImpl.java
@@ -1,5 +1,7 @@
 package egovframework.let.temp.service.impl;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Service;
 import egovframework.let.temp.service.TempService;
 import egovframework.let.temp.service.TempVO;
 import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
+import egovframework.rte.psl.dataaccess.util.EgovMap;
 
 @Service("tempService")
 public class TempServiceImpl extends EgovAbstractServiceImpl implements TempService{
@@ -25,4 +28,26 @@ public class TempServiceImpl extends EgovAbstractServiceImpl implements TempServ
 		public TempVO selectTemp(TempVO vo) throws Exception {
 			return tempMapper.selectTemp(vo);
 		}
+		
+		// 임시 데이터 목록 가져오기
+		public List<EgovMap> selectTempList(TempVO vo) throws Exception {
+			return tempMapper.selectTempList(vo);
+		}
+		
+		// 임시 데이터 등록하기
+		public String insertTemp(TempVO vo) throws Exception {
+			tempMapper.insertTemp(vo);
+			return null;
+		}
+		
+		// 임시 데이터 수정하기
+		public void updateTemp(TempVO vo) throws Exception {
+			tempMapper.updateTemp(vo);
+		}
+		
+		// 임시 데이터 삭제하기
+		public void deleteTemp(TempVO vo) throws Exception {
+			tempMapper.deleteTemp(vo);
+		}
+		
 }
